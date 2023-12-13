0cb89096b79dc5479d5e5cfdc731d41900e5c36a
AKADortys <thibault.a@outlook.com>
2023-12-13 13:12:15 +0100

    réorganisation classification du projet

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/index', function(req, res) {
+  res.render('pages/index');
+});
 
 // const db = mysql.createConnection(config);
 // db.connect((err) => {
@@ -27,7 +25,7 @@ app.set('view engine', 'ejs');
 //   }
 // });
 
-// Définissez routes ici..
+// Define your routes here...
 
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur le port ${port}`);
