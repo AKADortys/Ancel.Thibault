da11de6c411eb8dda373ff97108d6b1d3a74060c
AKADortys <thibault.a@outlook.com>
2024-01-10 08:27:19 +0100

    Ajout connexion à la BDD

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const config = require('./config');
 const ejs = require('ejs');
 const app = express();
 const port = 3000;
@@ -14,23 +13,22 @@ app.get('/index', function(req, res) {
   res.render('pages/index');
 });
 
-// const dbs = mysql.createConnection(config);
-// dbs.connect((err) => {
-//   if (err) {
-//     console.error('Erreur de connexion à la base de données:', err);
-//   } else {
-//     console.log('Connecté à la base de données MySQL');
-//   }
-// });
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.get('/utilisateurs', (req, res) => {
-//   dbs.query('SELECT * FROM utilisateur', (err, results) => {
-//      if (err) throw err;
-//      res.json(results);
-//   });
-// });
-// Define your routes here...
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'standard_user',
+  password: 'PWD',
+  database: 'QuizApp'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('Erreur de connexion à la base de données :', err);
+  } else {
+    console.log('Connexion à la base de données établie');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur le port ${port}`);
