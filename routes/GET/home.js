44d8747fba7b4ed10fbdf5d0a22592efec93b75b
AKADortys <thibault.a@outlook.com>
2024-06-01 23:04:40 +0200

    Ajout du formulaire de recherche sur le titre d'un quiz /home

--- routes/GET/home.js
+++ routes/GET/home.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 const { Utilisateur, Categorie, Quiz } = require('../../config/dbconnect');
 const CheckAuth = require('../../config/controller/CheckAuth');
+const { Op } = require('sequelize');
 
 router.get('/home', CheckAuth, async function (req, res) {
+  const pseudoUtilisateur = req.session.utilisateur.pseudo;
   try {
 
     //recupérer le pseudo utilisateur pour la nav bar
-    const pseudoUtilisateur = req.session.utilisateur.pseudo;
 
     //créer une liste des catégories et de leurs quizs asignés 
     let categorieQuizTable = [];
     const categories = await Categorie.findAll();
-    
+
     for (const categ of categories) {
       let object =
       {
@@ -24,29 +25,41 @@ router.get('/home', CheckAuth, async function (req, res) {
       };
       const quiz = await Quiz.findAll({ where: { id_categ: categ.id_categ } })
 
-      quiz.forEach((i)=>{ 
-         let objQuiz =
-         {
+      quiz.forEach((i) => {
+        let objQuiz =
+        {
           id_quiz: i.id_quiz,
           titre: i.titre,
           description: i.description,
           date: i.date_ajout,
           image: i.image.substring(7)
-         }
-         object.quiz.push(objQuiz);
+        }
+        object.quiz.push(objQuiz);
       })
 
       categorieQuizTable.push(object);
     }
 
     const utilisateurs = await Utilisateur.findAll();
+    const titre = req.query.titre;
+    // Recherche des quiz par titre
+    let quizzes = [];
+    if (titre) {
+      quizzes = await Quiz.findAll({
+        where: {
+          titre: {
+            [Op.like]: `%${titre}%`
+          }
+        }
+      });
+    }
 
     // Rendu de la vue EJS avec les données
-    res.render('home/index', {  categorieQuizTable, pseudoUtilisateur,utilisateurs });
+    res.render('home/index', { categorieQuizTable, pseudoUtilisateur, utilisateurs, quizzes });
   } catch (erreur) {
     const error = "Erreur lors de la récupération des données !"
     console.error('Erreur lors de la récupération des données :', erreur);
-    return res.render('home/Error', {error,pseudoUtilisateur})
+    return res.render('home/Error', { error, pseudoUtilisateur })
   }
 });
 
